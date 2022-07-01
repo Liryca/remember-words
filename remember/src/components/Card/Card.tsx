24fde7ea16fa789ee79c7eb127a389213000011a
Anna <tarasavahanna@gmail.com>
2022-07-01 11:16:38 +0300

    add state for DeleteModal in parent component

--- remember/src/components/Card/Card.tsx
+++ remember/src/components/Card/Card.tsx
@@ -5,41 +5,53 @@ import Control from '../Control/Mark';
 import speaker from './images/speaker.png';
 import edit from './images/edit.png';
 import trash from './images/trash.png';
-import {Dictionary } from '../../types/dictionaryTypes';
+import { Dictionary } from '../../types/dictionaryTypes';
 import { useLocation } from 'react-router-dom';
 import { useState } from 'react';
-import { sendDeletedWord } from '../../api/api'
+import DeleteModal from '../DeleteModal/DeleteModal';
 
 
 interface PropsType {
-    setWords: (x: any) => void;
+    setWords: (x: Dictionary) => void;
     words: Dictionary
     index: number,
-    setIndex: (x: any) => void
+    setIndex: (x: number) => void
 
 }
 
-const Card: React.FC<PropsType> = ({setWords, words, index, setIndex }) => {
+const Card: React.FC<PropsType> = ({ setWords, words, index, setIndex }) => {
 
-    const [show, setShow] = useState(false);
+    const [showChangeModal, setShowChangeModal] = useState(false);
+    const [showDeletModal, setShowDeleteModal] = useState(false);
+
     const path = useLocation().pathname;
     const divRef = React.useRef<HTMLDivElement | null>(null);
 
- 
     function handleModalOpen(e: React.MouseEvent) {
         e.stopPropagation();
-        setShow(true);
+        setShowChangeModal(true);
     }
-
-
-    console.log(show)
 
     const handleModalClose = (e: React.MouseEvent) => {
         const value = (e.target as HTMLInputElement).className;
         if (value === 'img-modal-close') {
-            setShow(false);
+            setShowChangeModal(false);
         }
     };
+
+
+    function handleDeleteModalOpen(e: React.MouseEvent) {
+        e.stopPropagation();
+        setShowDeleteModal(true);
+    }
+
+    const handleDeleteModalClose = (e: React.MouseEvent) => {
+        const value = (e.target as HTMLInputElement).className;
+        // if (value === 'img-modal-close') {
+            setShowDeleteModal(false);
+        // }
+    };
+
 
     function voiсe(e: React.MouseEvent) {
         e.stopPropagation()
@@ -55,17 +67,6 @@ const Card: React.FC<PropsType> = ({setWords, words, index, setIndex }) => {
         }
     }
 
-
-    function deleteCard(e: React.MouseEvent) {
-        e.stopPropagation();
-        setWords((prev: Dictionary) => prev.filter(i => i.id !== words[index].id))
-        if (words[index] === words[words.length - 1]) {
-            setIndex(0)
-        }
-        sendDeletedWord(words[index])
-
-    }
-
     return (
         <div className='wrapper-card'>
             <Control word={words[index]} setWords={setWords} />
@@ -73,36 +74,33 @@ const Card: React.FC<PropsType> = ({setWords, words, index, setIndex }) => {
                 <div ref={divRef} className='card' onClick={toggleCard}>
 
                     <div className="card__face card__face--front" >
-
                         <div className='card-control'>
-                        {path === '/learnWords' && <img src={speaker} alt='speker' onClick={voiсe} />}
-                            <img src={trash} alt='' onClick={deleteCard} />
+                            {path === '/learnWords' && <img src={speaker} alt='speker' onClick={voiсe} />}
+                            <img src={trash} alt='' onClick={handleDeleteModalOpen} />
                             <img src={edit} alt='edit' onClick={handleModalOpen} />
                         </div>
-
                         <div className='card-speaker'>
                             {path === '/repeatWords' ? <p> {words[index].rusVersion}</p> : <p>{words[index].englishVersion}</p>}
-                           
+
                         </div>
                     </div>
 
 
                     <div className="card__face card__face--back" >
-
                         <div className='card-control'>
-                        {path === '/repeatWords' && <img src={speaker} alt='speaker' onClick={voiсe} />}
-                          
-                            <img src={trash} alt='' onClick={deleteCard} />
-                            <img src={edit} alt='editor' onClick={handleModalOpen} />
+                            {path === '/repeatWords' && <img src={speaker} alt='speaker' onClick={voiсe} />}
+                            <img src={trash} alt='' onClick={handleDeleteModalOpen} />
+                            <img src={edit} alt='edit' onClick={handleModalOpen} />
                         </div>
                         <div className='card-speaker'>
                             {path === '/repeatWords' ? <p> {words[index].englishVersion}</p> : <p>{words[index].rusVersion}</p>}
-                          
                         </div>
                     </div>
                 </div>
             </div>
-            <Modal words={words} index={index} show={show} closeModal={handleModalClose} setWords={setWords} />
+            
+            <Modal words={words} setWords={setWords} index={index} show={showChangeModal} closeModal={handleModalClose}  />
+            <DeleteModal words={words}  setWords={setWords}  index={index}  setIndex={setIndex} show={showDeletModal}  setShow={setShowDeleteModal} closeModal={handleDeleteModalClose} />
         </div>
     )
 };
