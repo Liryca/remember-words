043c344cb6f4c49c2057e669729ea6243523ff74
Anna <tarasavahanna@gmail.com>
2022-07-04 12:25:12 +0300

    add new stylse in DeleteModal

--- remember/src/components/DeleteModal/DeleteModal.tsx
+++ remember/src/components/DeleteModal/DeleteModal.tsx
@@ -19,22 +19,25 @@ const DeleteModal: React.FC<PropsType>= ({ words, setWords,  index,setIndex, sho
 
     function deleteCard(e: React.MouseEvent) {
         e.stopPropagation();
+     
         setWords((prev: Dictionary) => prev.filter(i => i.id !== words[index].id))
         if (words[index] === words[words.length - 1]) {
             setIndex(0)
         }
-        sendDeletedWord(words[index]);
-
-        
+        setShow(false)
+        sendDeletedWord(words[index]); 
     }
 
     return (
         <div className='delete-modal'>
         <div className='wrapper-delete-modal' hidden={!show}>
-                <div className="delete-modal-background" onClick={closeModal}  >
-                    <p>You want to delete the card?</p>
-                    <button onClick={deleteCard}>Yes</button>
-                    <button onClick={closeModal}>No</button>
+                <div className="delete-modal-background">
+                    <div className='delete-modal-card'>
+                    <h2>You want to delete the card?</h2>
+                    <button className='delete-modal-button' onClick={deleteCard}>Yes</button>
+                    <button className='delete-modal-button' onClick={closeModal}>No</button>
+                    </div>
+                  
                 {/* <img className='img-modal-close' src={close} alt="close" /> */}
             </div>
         </div>
